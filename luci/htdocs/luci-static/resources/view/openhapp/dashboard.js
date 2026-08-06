1d18d93d2e94dec00a5f7c6aeae943feeda05773
valercha <63358623+valercha@users.noreply.github.com>
2026-08-06 21:52:53 +0200

    Polish LuCI dashboard view scaffold

--- luci/htdocs/luci-static/resources/view/openhapp/dashboard.js
+++ luci/htdocs/luci-static/resources/view/openhapp/dashboard.js
@@ -1,36 +1,21 @@
 'use strict';
 'require view';
-'require fs';
 'require uci';
 
 return view.extend({
 	title: _('OpenHapp Dashboard'),
 
 	load: function () {
-		return Promise.all([
-			uci.load('openhapp')
-		]);
+		return uci.load('openhapp');
 	},
 
 	render: function (data) {
-		var section = data[0] && data[0].openhapp ? data[0].openhapp.main : null;
+		var section = data && data.main ? data.main : null;
 		var enabled = section && section.enabled ? section.enabled : '0';
 		var engine = section && section.engine ? section.engine : 'xray';
 		var mode = section && section.mode ? section.mode : 'proxy';
 		var listen = section && section.listen ? section.listen : '127.0.0.1:0';
 		var subscription = section && section.subscription ? section.subscription : '';
-
-		var html = [];
-		html.push('<div class="cbi-map">');
-		html.push('<h2>OpenHapp Dashboard</h2>');
-		html.push('<div class="cbi-section">');
-		html.push('<p><strong>Runtime:</strong> ' + (enabled === '1' ? 'enabled' : 'disabled') + '</p>');
-		html.push('<p><strong>Engine:</strong> ' + engine + '</p>');
-		html.push('<p><strong>Mode:</strong> ' + mode + '</p>');
-		html.push('<p><strong>Listen:</strong> ' + listen + '</p>');
-		html.push('<p><strong>Subscription:</strong> ' + (subscription || 'none') + '</p>');
-		html.push('</div>');
-		html.push('</div>');
 
 		return E('div', { 'class': 'cbi-map' }, [
 			E('h2', {}, _('OpenHapp Dashboard')),
