41d90803c1f0e90e39138f8ac2192f654ca0ba26
Hacker <hacker@kali>
2026-08-16 21:11:35 +0300

    Improve LuCI Actions runtime status

--- luci/htdocs/luci-static/resources/view/openhapp/actions.js
+++ luci/htdocs/luci-static/resources/view/openhapp/actions.js
@@ -7,6 +7,11 @@ var callStatus = rpc.declare({
 	method: 'status'
 });
 
+var callEngineInfo = rpc.declare({
+	object: 'openhapp',
+	method: 'engine_info'
+});
+
 var callStart = rpc.declare({
 	object: 'openhapp',
 	method: 'start'
@@ -21,25 +26,38 @@ return view.extend({
 	title: _('OpenHapp Actions'),
 
 	load: function () {
-		return callStatus().catch(function () { return {}; });
+		return Promise.all([
+			callStatus().catch(function () { return {}; }),
+			callEngineInfo().catch(function () { return {}; })
+		]);
 	},
 
 	render: function (data) {
-		var status = data || {};
-		var runtimeState = status.running ? _('running') : _('stopped');
+		var status = data[0] || {};
+		var engineInfo = data[1] || {};
 		var busy = false;
+
+		var runtimeState = status.running ? _('running') : _('stopped');
+		var engineName = engineInfo.name || '-';
+		var engineState = engineInfo.running ? _('running') : _('stopped');
+		var engineAvailability = engineInfo.available ? _('available') : _('unavailable');
 
 		function runAction(action) {
 			if (busy)
 				return;
 
 			busy = true;
+
 			action()
 				.then(function () {
-					window.location.reload();
+					return window.location.reload();
 				})
 				.catch(function (err) {
-					window.alert(err && err.message ? err.message : _('Action failed'));
+					window.alert(
+						err && err.message
+							? err.message
+							: _('Action failed')
+					);
 				})
 				.finally(function () {
 					busy = false;
@@ -48,19 +66,42 @@ return view.extend({
 
 		return E('div', { 'class': 'cbi-map' }, [
 			E('h2', {}, _('OpenHapp Actions')),
+
 			E('div', { 'class': 'cbi-section' }, [
-				E('p', {}, [ E('strong', {}, _('Daemon state: ')), runtimeState ])
+				E('p', {}, [
+					E('strong', {}, _('OpenHapp daemon: ')),
+					runtimeState
+				]),
+				E('p', {}, [
+					E('strong', {}, _('Engine: ')),
+					engineName
+				]),
+				E('p', {}, [
+					E('strong', {}, _('Engine state: ')),
+					engineState
+				]),
+				E('p', {}, [
+					E('strong', {}, _('Engine availability: ')),
+					engineAvailability
+				])
 			]),
+
 			E('div', { 'class': 'cbi-section' }, [
 				E('button', {
 					'class': 'btn cbi-button cbi-button-action',
-					'click': function () { runAction(callStart); }
-				}, _('Start')),
+					'click': function () {
+						runAction(callStart);
+					}
+				}, _('Start OpenHapp')),
+
 				' ',
+
 				E('button', {
 					'class': 'btn cbi-button cbi-button-reset',
-					'click': function () { runAction(callStop); }
-				}, _('Stop'))
+					'click': function () {
+						runAction(callStop);
+					}
+				}, _('Stop OpenHapp'))
 			])
 		]);
 	}
